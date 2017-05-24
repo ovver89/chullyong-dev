7833e50cf5dba1448b552f6983285ac1732840b9
chullyongsin <chullyongsin@gmail.com>
2017-05-24 17:39:34 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,20 +1,44 @@
-function populate(){
+function populate() {
   if(quiz.isEnded()) {
-    //showScores();
+    showScores();
   }
   else {
     // show question
     var element = document.getElementById("question");
     element.innerHTML = quiz.getQuestionIndex().text;
+
+    var choices = quiz.getQuestionIndex().choices;
+    for(var i =0; i< choices.length; i++) {
+      var element = document.getElementById("choice" + i);
+      element.innerHTML = choices[i];
+      quess("btn" + i, choices[i]);
+
+    }
+  }
+};
+
+function guess(id, guess){
+  var button = document.getElementById(id);
+  button.onclick = function(){
+    quiz.guess(guess);
+    populate();
   }
 }
 
+function showScores() {
+    var gameOverHtml = "<h1>Result</h1>";
+        gameOverHtml += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+        var element = document.getElementById("quiz");
+        elemnt.innerHTML = gameOverHtml;
+}
+
+
 var questions = [
-  new Questions("");
-  new Questions("");
-  new Questions("");
-  new Questions("");
-  new Questions("");
+  new Questions("adsfasdfsadf?", ["heel", "b", "c", "d"], "c"),
+  new Questions("adsfasdfsadf?", ["heel", "b", "c", "d"], "c"),
+  new Questions("adsfasdfsadf?", ["heel", "b", "c", "d"], "c"),
+  new Questions("adsfasdfsadf?", ["heel", "b", "c", "d"], "c"),
+  new Questions("adsfasdfsadf?", ["heel", "b", "c", "d"], "c"),
 ];
 
 var quiz = new Quiz(questions);
