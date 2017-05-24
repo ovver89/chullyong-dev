e38f9ef85ef9da4fd9c35622803c1400ac001100
chullyongsin <chullyongsin@gmail.com>
2017-05-24 18:38:49 +0900

    df
    df

--- question.js
+++ question.js
@@ -1,10 +1,10 @@
-function Question(text, choice, answer) {
+function Question(text, choices, answer) {
   this.text= text;
   this.choices = choices;
   this.answer = answer;
 }
 
-Question .prototype.correctAnswer = function(choice) {
+Question.prototype.correctAnswer = function(choice) {
   return choice === this.answer;
 
 }
